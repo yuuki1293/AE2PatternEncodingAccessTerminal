f14c06dce7f10abbd76acf82aa8bbee899800848
yuuki1293 <yuuki1293@outlook.jp>
2025-10-28 00:42:22 +0900

    chore: remove brace

--- src/main/java/yuuki1293/ae2peat/definisions/PEATItems.java
+++ src/main/java/yuuki1293/ae2peat/definisions/PEATItems.java
@@ -6,9 +6,11 @@ import appeng.items.parts.PartItem;
 import net.minecraft.world.item.Item;
 import net.pedroksl.ae2addonlib.registry.ItemRegistry;
 import net.pedroksl.ae2addonlib.registry.helpers.LibItemDefinition;
+import net.pedroksl.ae2addonlib.util.AddonEnum;
 import yuuki1293.ae2peat.AE2PEAT;
 import yuuki1293.ae2peat.wireless.ItemWPEAT;
 import yuuki1293.ae2peat.parts.PatternEncodingAccessTerminalPart;
+import yuuki1293.ae2peat.xmod.Addons;
 
 import java.util.function.Function;
 
@@ -20,11 +22,31 @@ public class PEATItems extends ItemRegistry {
     }
 
     public static final LibItemDefinition<PartItem<PatternEncodingAccessTerminalPart>> PATTERN_ENCODING_ACCESS_TERMINAL = part(
-        "ME Pattern Encoding Access Terminal", PEATIds.PATTERN_ENCODING_ACCESS_TERMINAL, PatternEncodingAccessTerminalPart.class, PatternEncodingAccessTerminalPart::new
+        "ME Pattern Encoding Access Terminal",
+        PEATIds.PATTERN_ENCODING_ACCESS_TERMINAL,
+        PatternEncodingAccessTerminalPart.class,
+        PatternEncodingAccessTerminalPart::new
     );
-    public static final LibItemDefinition<ItemWPEAT> WIRELESS_PATTERN_ENCODING_ACCESS_TERMINAL = item(
-        "ME Wireless Pattern Encoding Access Terminal", PEATIds.WIRELESS_PATTERN_ENCODING_ACCESS_TERMINAL, (p) -> new ItemWPEAT()
+    public static final LibItemDefinition<ItemWPEAT> WIRELESS_PATTERN_ENCODING_ACCESS_TERMINAL = conditionalItem(
+        Addons.AE2WTLIB,
+        "ME Wireless Pattern Encoding Access Terminal",
+        PEATIds.WIRELESS_PATTERN_ENCODING_ACCESS_TERMINAL,
+        "yuuki1293.ae2peat.wireless.ItemWPEAT"
     );
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Item> LibItemDefinition<T> conditionalItem(
+        AddonEnum addon, String englishName, String id, String itemClass) {
+        if (addon.isLoaded()) {
+            try {
+                var instance =
+                    (T) Class.forName(itemClass).getDeclaredConstructor().newInstance();
+                return item(AE2PEAT.MOD_ID, englishName, id, p -> instance);
+            } catch (Exception ignored) {
+            }
+        }
+        return null;
+    }
 
     protected static <T extends Item> LibItemDefinition<T> item(
         String englishName, String id, Function<Item.Properties, T> factory) {
