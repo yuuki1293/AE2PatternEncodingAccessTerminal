9d01159aef932b3cd93f87b3fd2b432a44f10015
yuuki1293 <yuuki1293@outlook.jp>
2025-10-29 12:49:23 +0900

    chore: add wt recipe

--- src/main/java/yuuki1293/ae2peat/datagen/PEATRecipeProvider.java
+++ src/main/java/yuuki1293/ae2peat/datagen/PEATRecipeProvider.java
@@ -1,15 +1,16 @@
 package yuuki1293.ae2peat.datagen;
 
+import appeng.core.definitions.AEBlocks;
+import appeng.core.definitions.AEItems;
 import appeng.core.definitions.AEParts;
 import appeng.datagen.providers.tags.ConventionTags;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import org.jetbrains.annotations.NotNull;
 import yuuki1293.ae2peat.AE2PEAT;
 import yuuki1293.ae2peat.definisions.PEATItems;
@@ -22,10 +23,23 @@ public class PEATRecipeProvider extends RecipeProvider {
     @Override
     protected void buildRecipes(@NotNull Consumer<FinishedRecipe> c) {
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, PEATItems.PATTERN_ENCODING_ACCESS_TERMINAL)
-                .requires(AEParts.PATTERN_ACCESS_TERMINAL)
-                .requires(AEParts.PATTERN_ENCODING_TERMINAL)
-                .unlockedBy("has_pattern_provider", has(ConventionTags.PATTERN_PROVIDER))
-                .unlockedBy("has_crafting_terminal", has(AEParts.CRAFTING_TERMINAL))
-                .save(c, AE2PEAT.makeId("network/parts/terminals_pattern_encoding"));
+            .requires(AEParts.PATTERN_ACCESS_TERMINAL)
+            .requires(AEParts.PATTERN_ENCODING_TERMINAL)
+            .unlockedBy("has_pattern_provider", has(ConventionTags.PATTERN_PROVIDER))
+            .unlockedBy("has_crafting_terminal", has(AEParts.CRAFTING_TERMINAL))
+            .save(c, AE2PEAT.makeId("network/parts/pattern_encoding_access_terminal"));
+        if (PEATItems.WIRELESS_PATTERN_ENCODING_ACCESS_TERMINAL != null) {
+            ShapedRecipeBuilder.shaped(RecipeCategory.MISC, PEATItems.WIRELESS_PATTERN_ENCODING_ACCESS_TERMINAL)
+                .pattern("a")
+                .pattern("b")
+                .pattern("c")
+                .define('a', AEItems.WIRELESS_RECEIVER)
+                .define('b', PEATItems.PATTERN_ENCODING_ACCESS_TERMINAL)
+                .define('c', AEBlocks.DENSE_ENERGY_CELL)
+                .unlockedBy("has_peat_terminal", has(PEATItems.PATTERN_ENCODING_ACCESS_TERMINAL))
+                .unlockedBy("has_dense_energy_cell", has(AEBlocks.DENSE_ENERGY_CELL))
+                .unlockedBy("has_wireless_receiver", has(AEItems.WIRELESS_RECEIVER))
+                .save(c, AE2PEAT.makeId("network/wireless_pattern_encoding_access_terminal"));
+        }
     }
 }
