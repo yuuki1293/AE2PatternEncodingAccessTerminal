f0d97ae2f077992fcaef40875d59300cd865197d
yuuki1293 <yuuki1293@outlook.jp>
2025-10-31 00:49:28 +0900

    rei

--- src/main/java/yuuki1293/ae2peat/integration/modules/rei/REIPlugin.java
+++ src/main/java/yuuki1293/ae2peat/integration/modules/rei/REIPlugin.java
@@ -2,13 +2,14 @@ package yuuki1293.ae2peat.integration.modules.rei;
 
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
+import me.shedaniel.rei.forge.REIPluginClient;
 import mezz.jei.api.JeiPlugin;
 import yuuki1293.ae2peat.integration.modules.rei.transfer.EncodePatternTransferHandler;
 import yuuki1293.ae2peat.menu.PatternEncodingAccessTermMenu;
 import yuuki1293.ae2peat.wireless.WPEATMenu;
 
 @SuppressWarnings("unused")
-@JeiPlugin
+@REIPluginClient
 public class REIPlugin implements REIClientPlugin {
     @Override
     public void registerTransferHandlers(TransferHandlerRegistry registry) {
